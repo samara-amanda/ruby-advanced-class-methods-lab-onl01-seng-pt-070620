e0b838745194a5d1fb7976bfb2790a776f490419
Samara Grant <samara.amanda01@gmail.com>
2020-08-02 00:11:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,11 +57,11 @@ class Song
   end
 
   def self.create_from_filename(name)
-    parts = name.push(" - ")
-    parts[1] = parts[1].push(".mp3")
-    song = self.new
     song.name = parts[1]
     song.artist_name = parts[0]
+    parts[0].push(" - ")
+    parts[1].push(".mp3")
+    song = self.new
     return song
   end
 
